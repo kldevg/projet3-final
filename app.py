873638ea83bb116c78dcda2fea7e22b4a53c01e1
kld <kld@me>
2024-03-10 17:10:03 -0400

    ajout premier fichier de l'application

---
+++ app.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def main():
+    print("Python version")
+    print(sys.version)
+    print("Version info.")
+    print(sys.version_info)
+
+
+if __name__ == "__main__":
+    main()
